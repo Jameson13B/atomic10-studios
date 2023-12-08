c97d52d5ca5ccfe4c2345808efeede8f1a67e14b
Jameson Brown <jameson.brown@mx.com>
2023-12-08 09:50:41 -0700

    Updated styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 function App() {
   return (
     <div className="flex flex-col justify-center items-center bg-[url('/src/assets/neon-background-dark.jpg')] bg-cover bg-no-repeat h-full w-full">
-      <h1 className="font-chicle text-[white] text-7xl text-center bg-[rgba(0,0,0,0.35)] p-[12px] rounded-[20px]">
+      <h1 className="font-chicle text-[white] text-7xl text-center text-shadow p-[16px] rounded-[20px] mx-auto">
         Atomic10 Studios
       </h1>
       <hr className="w-[50%] sm:w-[20%] border-double border-t-8 border-[white] rounded-[20px] my-[30px]" />
-      <h2 className="font-chicle text-[white] text-4xl">SL,UT</h2>
+      <h2 className="font-chicle text-[white] text-4xl text-shadow p-[16px] rounded-[20px]">
+        SL,UT
+      </h2>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      boxShadow: {
+        button:
+          '0 0 5px #03e9f4, 0 0 25px #03e9f4, 0 0 50px #03e9f4, 0 0 200px #03e9f4',
+      },
       fontFamily: {
         chicle: ['Chicle', 'serif'],
       },
+      hueRotate: {
+        270: '270deg',
+        110: '110deg',
+      },
+      textShadow: {
+        DEFAULT: '4px 6px 0 rgba(0, 0, 0, 0.4)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
