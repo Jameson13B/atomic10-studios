117e7a68820e8863f2edbb96fefcb0000b2473c4
Jameson Brown <jamesonbrown@neolivity.com>
2025-03-25 21:23:07 -0600

    Added values

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import NeonLogo from "../assets/neon_logo.png"
 export const About = () => {
   return (
     <div
-      className="w-full h-[calc(100vh-96px)] bg-[#191919] px-[25px] pb-[100px] pt-[55px]"
+      className="w-full min-h-[calc(100vh-96px)] bg-[#191919] px-[25px] pb-16 pt-[55px]"
       id="about-section"
     >
       <div className="flex flex-col max-w-[650px] h-full mx-auto">
@@ -19,25 +19,43 @@ export const About = () => {
           curiosity, we're in.
         </p>
         <p className="text-[#C5C5C5] text-[14px] font-[400] font-Poppins mb-[40px] text-left">
-          At the heart of it all is a simple mission: to create, explore, and
-          share. Whether crafting sleek digital experiences, pushing the
+          At the heart of it all is a simple mission: We follow curiosity
+          wherever it leads—building bold ideas and experimenting just because
+          we can. Whether crafting sleek digital experiences, pushing the
           boundaries of technology, or hosting unforgettable nights filled with
           food and community, every Atomic10 project is driven by passion,
           collaboration, and a relentless pursuit of what's next.
         </p>
         <p className="text-[#C5C5C5] text-[14px] font-[400] font-Poppins mb-[40px] text-left">
           We're not here to follow the rules—we're here to redefine them.
-          Welcome to Atomic10.
+          <span className="font-[600]"> Welcome to Atomic10!</span>
         </p>
         <p className="text-[#C5C5C5] text-[14px] font-[800] font-Poppins mb-[22px]">
           Headquarters: <span className="font-[600]">Salt Lake City, UT</span>
         </p>
-        <p className="text-[#C5C5C5] text-[14px] mb-[60px] font-[800] font-Poppins">
+        <p className="text-[#C5C5C5] text-[14px] mb-[24px] sm:mb-[60px] font-[800] font-Poppins">
           Contact:{" "}
           <a className="font-[600]" href="mailto: jameson13b@icloud.com">
             jameson13b@icloud.com
           </a>
         </p>
+        <div className="flex flex-col md:flex-row justify-evenly">
+          <h2 className="text-[#8491A0] text-[35px] font-[700] font-Poppins mb-0 md:mb-[30px] text-center md:text-left uppercase opacity-30 blur-[1px] leading-[100%]">
+            Passion
+          </h2>
+          <span className="hidden md:block text-[#8491A0] text-[35px] font-[700] font-Poppins mb-0 md:mb-[30px] text-center md:text-left uppercase opacity-50 blur-[1px] leading-[100%]">
+            |
+          </span>
+          <h2 className="text-[#8491A0] text-[35px] font-[700] font-Poppins mb-0 md:mb-[30px] text-center md:text-left uppercase opacity-30 blur-[1px] leading-[100%]">
+            Innovation
+          </h2>
+          <span className="hidden md:block text-[#8491A0] text-[35px] font-[700] font-Poppins mb-0 md:mb-[30px] text-center md:text-left uppercase opacity-50 blur-[1px] leading-[100%]">
+            |
+          </span>
+          <h2 className="text-[#8491A0] text-[35px] font-[700] font-Poppins mb-[30px] text-center md:text-left uppercase opacity-30 blur-[1px] leading-[100%]">
+            Curiosity
+          </h2>
+        </div>
         <img
           src={NeonLogo}
           alt="logo"
